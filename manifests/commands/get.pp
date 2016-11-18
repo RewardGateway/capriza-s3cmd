1a6f9567c64e33df5b6df2d796d15e5de13a93ea
Atanas Atanasov <atanas.atanasov@rewardgateway.com>
2016-11-18 14:33:19 +0200

    correct manifests syntax with lint

--- manifests/commands/get.pp
+++ manifests/commands/get.pp
@@ -1,20 +1,20 @@
+# Get class
 define s3cmd::commands::get(
   $s3_object,
   $owner,
   $cwd
 ) {
-
-  $homedir = $owner ? {
-    root => '/root',
-    default => "/home/$owner",
-  }
-
-  exec{$name:
-     command => "s3cmd get -c ${homedir}/.s3cfg ${s3_object} ${name}",
-     path=>'/usr/bin',
-     cwd => $cwd,
-     user => $owner,
-     creates => $name,
-     require => Class['s3cmd::config','s3cmd::install'],
-  }
+    $homedir = $owner ? {
+        root    => '/root',
+        default => "/home/${owner}",
+    }
+    $testvar = $name
+    exec { $name :
+        command => "s3cmd get -c ${homedir}/.s3cfg ${s3_object} ${name}",
+        path    => '/usr/bin',
+        cwd     => $cwd,
+        user    => $owner,
+        creates => $name,
+        require => Class['s3cmd::config','s3cmd::install'],
+    }
 }
