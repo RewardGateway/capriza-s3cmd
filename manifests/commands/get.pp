0e7ead27cf2d1adec5004a1d9ba7ea43479a7df1
Atanas Atanasov <atanas.atanasov@rewardgateway.com>
2016-11-22 15:44:29 +0200

    fix paths based on diff provider install

--- manifests/commands/get.pp
+++ manifests/commands/get.pp
@@ -11,7 +11,7 @@ define s3cmd::commands::get(
     $testvar = $name
     exec { $name :
         command => "s3cmd get -c ${homedir}/.s3cfg ${s3_object} ${name}",
-        path    => '/usr/bin',
+        path    => ['/usr/local/bin','/usr/bin'],
         cwd     => $cwd,
         user    => $owner,
         creates => $name,
