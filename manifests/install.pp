6e5cf917b249c26b77d9518a8fd2937e3d875569
Atanas Atanasov <atanas.atanasov@rewardgateway.com>
2016-11-22 17:45:15 +0200

    restore os detection if section

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,6 +3,14 @@ class s3cmd::install (
     $s3cmd_version,
     $s3cmd_provider,
     ){
+    if $::osfamily == 'redhat' {
+        file {'/etc/yum.repos.d/s3tools.repo':
+            source => 'puppet:///modules/s3cmd/etc/yum.repos.d/s3tools.repo',
+        }
+        $s3cmd_require = File['/etc/yum.repos.d/s3tools.repo']
+    } else {
+        $s3cmd_require = undef
+    }
     package { 's3cmd' :
         ensure   => $s3cmd_version,
         provider => $s3cmd_provider,
