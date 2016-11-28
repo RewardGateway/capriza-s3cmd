11f63cba179f7c7663fafa7e9ed8a2ef800d066e
Atanas Atanasov <nikkonix@gmail.com>
2016-11-28 16:20:38 +0200

    Optimise s3cmd puppet module (#1)
    * correct manifests syntax with lint
    * improve init/install class, using ver and provider as vars

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,22 @@
+# Main class
 class s3cmd (
-  $aws_access_key,
-  $aws_secret_key,
-  $gpg_passphrase,
-  $owner,
+    $aws_access_key,
+    $aws_secret_key,
+    $gpg_passphrase,
+    $owner,
+    $s3cmd_version  = latest,
+    $s3cmd_provider = 'apt',
 )
 {
-  class{'s3cmd::install':}
-  class{'s3cmd::config':
-      aws_access_key => $aws_access_key,
-      aws_secret_key => $aws_secret_key,
-      gpg_passphrase => $gpg_passphrase,
-      owner => $owner,
-      require => Class['s3cmd::install'],
-  }
+    class{'s3cmd::install':
+        s3cmd_version  => $s3cmd_version,
+        s3cmd_provider => $s3cmd_provider,
+    }
+    class{'s3cmd::config':
+        aws_access_key => $aws_access_key,
+        aws_secret_key => $aws_secret_key,
+        gpg_passphrase => $gpg_passphrase,
+        owner          => $owner,
+        require        => Class['s3cmd::install'],
+    }
 }
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,17 +1,18 @@
-class s3cmd::install (){
-
-  if $osfamily == 'redhat' {
-    file {'/etc/yum.repos.d/s3tools.repo':
-      source => 'puppet:///modules/s3cmd/etc/yum.repos.d/s3tools.repo',
+# Install class
+class s3cmd::install (
+    $s3cmd_version,
+    $s3cmd_provider,
+    ){
+    if $::osfamily == 'redhat' {
+        file {'/etc/yum.repos.d/s3tools.repo':
+            source => 'puppet:///modules/s3cmd/etc/yum.repos.d/s3tools.repo',
+        }
+        $s3cmd_require = File['/etc/yum.repos.d/s3tools.repo']
+    } else {
+        $s3cmd_require = undef
     }
-    $s3cmd_require = File['/etc/yum.repos.d/s3tools.repo']
-  } else {
-    $s3cmd_require = undef
-  }
-
-  package {'s3cmd':
-      ensure => installed,
-      require =>  $s3cmd_require,
-  }
-
+    package { 's3cmd' :
+        ensure   => $s3cmd_version,
+        provider => $s3cmd_provider,
+    }
 }
